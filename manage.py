34a60ffa79944e0e4b3abfece97ce2b9d92bc97b
Ryan Kaiser <ryandkaiser@gmail.com>
2016-09-08 15:48:21 -0400

    modify manage.py for simple tests

--- manage.py
+++ manage.py
@@ -3,7 +3,16 @@ import os
 import sys
 
 if __name__ == "__main__":
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agcs.settings")
+
+    if 'test' in sys.argv and sys.argv.index('test') == 1:
+        os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.settings'
+        if ('-p' or '--pattern') not in sys.argv:
+            sys.argv.insert(2, '-p')
+            sys.argv.insert(3, '*.py')
+        if 'tests' not in sys.argv:
+            sys.argv.append('tests')
+    else:
+        os.environ.setdefault("DJANGO_SETTINGS_MODULE", "agcs.settings")
 
     from django.core.management import execute_from_command_line
 
