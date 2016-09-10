f00bb0e53942d583e395d968b4f1f5b8af58872d
Ryan Kaiser <ryandkaiser@gmail.com>
2016-09-09 22:39:07 -0400

    use postgres for travis; switch to codecov

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-.PHONY: all test clean upload
+.PHONY: all install upgrade coverage html travis test clean upload
 
 clean:
 	find . -name "*.pyc" -type f -delete
@@ -6,5 +6,23 @@ clean:
 	find . -name "*.egg-info" -type d -exec rm -rf {} \; || true
 	rm -rf coverage_html_report .coverage
 
+install:
+	pip install -U setuptools
+	pip install -q psycopg2
+	pip install -r requirements.txt
+	pip install anglerfish
+	pip install css-html-js-minify
+	pip install codecov
+
+coverage:
+	coverage run --source=landing,agcs runtests.py
+
+html: coverage
+	coverage html
+
+travis: coverage
+
 test:
 	python runtests.py
+
+
--- tests/settings.py
+++ tests/settings.py
@@ -8,17 +8,34 @@ INSTALLED_APPS += ["tests",]
 RECAPTCHA_PRIVATE_KEY = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe'
 RECAPTCHA_PUBLIC_KEY = '6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI'
 
-if (os.environ.get('TRAVIS_CL_TEST') or not
-    locals().get('DATABASES')
-): DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'django_db',
-     },
-}
-
 if locals().get('LOGGING'):
     del(LOGGING)
+
+if os.environ.get('TRAVIS_CL_TEST'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'agcs_db',
+            'USER': 'postgres',
+       },
+   }
+elif not locals().get('DATABASES'):
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'agcs_db',
+         },
+    }
+
+CACHES = {
+    'default': {
+        'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
+    },
+    'machina_attachments': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/tmp',
+    }
+}
 
 STATIC_URL = '/static/'
 STATIC_ROOT = '/tmp'
