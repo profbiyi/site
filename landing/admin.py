37d9bf5e55a7b1bb62ffeb17d6ef139943a827b1
Ryan Kaiser <ryandkaiser@gmail.com>
2016-09-15 19:39:17 -0500

    add move_to_bottom action

--- landing/admin.py
+++ landing/admin.py
@@ -39,36 +39,52 @@ class ServiceAdmin(admin.ModelAdmin):
     list_display = ('name', 'order',)
     ordering = ('order',)
     readonly_fields = ('order', 'html',)
-    actions = ['move_to_top']
+    actions = ['move_to_top', 'move_to_bottom']
 
     def move_to_top(self, request, queryset):
+        self.move_to_x('top', request, queryset)
+
+    def move_to_bottom(self, request, queryset):
+        self.move_to_x('bottom', request, queryset)
+
+    def move_to_x(self, where, request, queryset):
         if len(queryset) > 1:
             self.message_user(request,
                 'Please select only one service'
             )
             return
 
-        new_first = queryset.last()
+        selection = queryset.last()
 
-        if new_first.order == 1:
+        target = (where == 'top') and 1 or (
+            Service.objects.order_by('order').last().order
+        )
+
+        if selection.order == target:
             self.message_user(request,
-                '%s is already at the top!' % new_first.name
-            )
+                '%s is already at the %s!' % (
+                new_first.name, where
+            ))
             return
 
-        Service.objects.filter(
-            order__lt=new_first.order
-        ).update(order=F('order') + 1)
+        if where == 'top':
+            Service.objects.filter(
+                order__lt=selection.order
+            ).update(order=F('order') + 1)
+        else:
+            Service.objects.filter(
+                order__gt=selection.order
+            ).update(order=F('order') - 1)
 
-        new_first.order = 1
-        new_first.save()
+        selection.order = target
+        selection.save()
 
         self.message_user(request,
-            'Successfully moved %s to the top' % new_first.name
+            'Successfully moved %s to the top' % selection.name
         )
 
     move_to_top.short_description    = 'Move Service to top'
-
+    move_to_bottom.short_description = 'Move Service to bottom'
 
 @admin.register(Contact)
 class ContactAdmin(admin.ModelAdmin):
