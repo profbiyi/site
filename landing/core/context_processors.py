33e6eec046d5988540623e644841fc98438e004c
Ryan Kaiser <ryank@alphageek.xyz>
2016-09-28 04:23:22 -0500

    Remove extra.json and all references thereto

--- landing/core/context_processors.py
+++ landing/core/context_processors.py
@@ -7,16 +7,9 @@ def extra(request):
        'favicon_prefix': settings.FAVICON_PREFIX
     } if getattr(settings, 'FAVICON_PREFIX', None) else {}
 
-    try:
-        with settings.BASE_DIR.joinpath('context.json').open() as handle:
-            extra.update(json.load(handle))
-    except IOError: # pragma: no cover
-        pass
-
     return { 'extra': extra }
 
 def services(request):
     return {
         'service_list': Service.objects.all(),
     }
-
